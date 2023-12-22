3031ddf75554929a8df2343ac281d41cec6900b8
1910631170019_FarhanAssegaf <82705668+fhnasgf28@users.noreply.github.com>
2023-12-22 17:08:37 +0700

    Add files via upload

---
+++ databuku.sql
@@ -0,0 +1,10 @@
+INSERT INTO nama_tabel (id, harga, judul, penerbit, penulis, tahun_terbit)
+VALUES 
+('0', 100000, 'Harry Potter dan Batu BertuaH', 'Bentang Pustaka', 'J.K. Rowling', 2001),
+('1', 75000, 'Perahu KertaS', 'Gramedia Pustaka Utama', 'Dewi Lestari', 2009),
+('2', 50000, 'Laskar Pelangi', 'Bentang Pustaka', 'Andrea Hirata', 2005),
+('3', 55000, 'Dilan 1990', 'Mizan Media Utama', 'Pidi Baiq', 2014),
+('4', 70000, 'Filosofi Teras', 'Gramedia Pustaka Utama', 'Henry Manampiring', 2018),
+('5', 65000, 'Cinta di Balik Awan', 'Gramedia Pustaka Utama', 'Tere Liye', 2018),
+('6', 75000, 'Sebuah Seni untuk Bersikap Bodo Amat', 'Gramedia Pustaka Utama', 'Mark Manson', 2018),
+('-NmFu2cEzJih4Ld7SLRh', 10000, 'MTK', 'JOGJA', 'assegaf', '2010');
